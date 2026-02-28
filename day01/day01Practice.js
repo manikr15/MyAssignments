5441222531a3408cf55fd2756b24f9202cd0d351
Manikandan R <31146671+manikr15@users.noreply.github.com>
2026-02-28 23:37:08 +0530

    Add files via upload
    day01

---
+++ day01/day01Practice.js
@@ -0,0 +1,14 @@
+
+var firstName="Manik";
+var companyName="Amazon";
+var mobileNumber=7401012945;
+var isAutomation=true;
+var hasPlayWright;
+
+console.log("First Name is: " + firstName + " and datatype is: " + typeof firstName);
+console.log("Company Name is: " + companyName + " and datatype is: " + typeof companyName);
+console.log("Mobile Number is: " + mobileNumber + " and datatype is: " + typeof mobileNumber);
+console.log("Automation Enrolled: " + isAutomation + " and datatype is: " + typeof isAutomation);
+console.log("Installed Playwright: " + hasPlayWright + " and datatype is: " + typeof hasPlayWright);
+
+
