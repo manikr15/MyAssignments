7c20977e47e24226b9932b7df3916cea5325f089
Manikandan R <31146671+manikr15@users.noreply.github.com>
2026-03-01 18:41:43 +0530

    Add files via upload

---
+++ week01/day02/day02Practice.js
@@ -0,0 +1,33 @@
+
+for(let i =1;i <=20;i++)
+{
+    if(i%2!=0)
+    {
+        console.log("Print odd number"+i)
+    }
+}
+
+/*
+let i=1;
+while(i<=20)
+{
+    if(i%2!=0)
+    {
+        console.log("Print odd number"+i)
+    }
+    i++;
+}
+*/
+
+/*
+let i=1;
+do
+{
+    if(i%2!=0)
+    {
+        console.log("Print odd number"+i)
+    }
+    i++;
+}while(i<=20) 
+    
+*/
