7e69099831082bc90c181ff92566d475d3b6414b
Manikandan R <31146671+manikr15@users.noreply.github.com>
2026-03-06 18:37:27 +0530

    Update day01Practice.js

--- week01/day01/day01Practice.js
+++ week01/day01/day01Practice.js
@@ -1,4 +1,5 @@
 //week01 day01 Breakout session test
+// Print a variable and its datatype declared with var keyword
 
 var firstName="Manik";
 var companyName="Amazon";
@@ -11,6 +12,7 @@ console.log("Company Name is: " + companyName + " and datatype is: " + typeof co
 console.log("Mobile Number is: " + mobileNumber + " and datatype is: " + typeof mobileNumber);
 console.log("Automation Enrolled: " + isAutomation + " and datatype is: " + typeof isAutomation);
 console.log("Installed Playwright: " + hasPlayWright + " and datatype is: " + typeof hasPlayWright);
+
 
 
 
