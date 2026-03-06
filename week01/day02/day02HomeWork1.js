ace62f6359202ca494fb4b84d988ea3a60ffb45f
Manikandan R <31146671+manikr15@users.noreply.github.com>
2026-03-06 18:27:36 +0530

    Add files via upload

---
+++ week01/day02/day02HomeWork1.js
@@ -0,0 +1,13 @@
+function numType(number) {
+if(number>0){
+    console.log("The number is positive");
+} else if(number<0){
+    console.log("The number is negative");  
+} else {
+    console.log("The number is neutral");
+}
+}
+
+numType(5);
+numType(-3);
+numType(0);
---
+++ week01/day02/day02HomeWork2.js
@@ -0,0 +1,39 @@
+function launchBrowser(browser) {
+    if (browser === "chrome") {
+        console.log("Launching Chrome browser...");
+    } else if (browser === "firefox") {
+        console.log("Launching Firefox browser...");
+    } else if (browser === "safari") {
+        console.log("Launching Safari browser...");
+    } else if (browser === "Edge") {
+        console.log("Launching Edge browser...");
+    } else {
+        console.log("Browser not supported.");
+    }
+}
+
+function runTests(string) {
+    switch (string) {
+        case "smoke":
+            console.log("Running Smoke Test ...");
+            break;
+        case "sanity":
+            console.log("Running Sanity Test ...");
+            break;
+        case "regression":
+            console.log("Running Regression Test ...");
+            break;
+        default:
+            console.log("Running Smoke Test...");
+    }
+}
+
+launchBrowser("chrome");
+launchBrowser("firefox");
+launchBrowser("safari");
+launchBrowser("Edge");
+launchBrowser("Opera");
+runTests("smoke");
+runTests("sanity");
+runTests("regression");
+runTests("UAT");
