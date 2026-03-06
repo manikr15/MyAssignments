f403da7cfe326f971fb36d5cf1b3d9ecb8d0e20e
Manikandan R <31146671+manikr15@users.noreply.github.com>
2026-03-06 18:26:13 +0530

    Add files via upload

---
+++ week01/day01/day01HomeWork.js
@@ -0,0 +1,14 @@
+
+let firstName="Manik";
+let companyName="Amazon";
+let mobileNumber=7401012945;
+let isAutomation=true;
+let hasPlayWright;
+
+console.log("First Name is: " + firstName + " and datatype is: " + typeof firstName);
+console.log("Company Name is: " + companyName + " and datatype is: " + typeof companyName);
+console.log("Mobile Number is: " + mobileNumber + " and datatype is: " + typeof mobileNumber);
+console.log("Automation Enrolled: " + isAutomation + " and datatype is: " + typeof isAutomation);
+console.log("Installed Playwright: " + hasPlayWright + " and datatype is: " + typeof hasPlayWright);
+
+
